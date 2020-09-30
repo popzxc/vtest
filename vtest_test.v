56e71ec35209bf531db0509691eed936c12a826e
Lukas Neubert <serkonda@protonmail.com>
2020-09-30 15:21:19 +0200

    make test pass

--- vtest_test.v
+++ vtest_test.v
@@ -1,24 +1,25 @@
-import vtest
+module vtest
+
 import term
 
 fn test_asserts() {
-    assert !vtest.assert_eq_int(1, 2)
-    assert vtest.assert_eq_int(1, 1)
+    assert !assert_eq_int(1, 2)
+    assert assert_eq_int(1, 1)
 
-    assert !vtest.assert_eq_str("abc", "cde")
-    assert vtest.assert_eq_str("abc", "abc")
+    assert !assert_eq_str("abc", "cde")
+    assert assert_eq_str("abc", "abc")
 
-    assert !vtest.assert_true(false)
-    assert vtest.assert_true(true)
+    assert !assert_true(false)
+    assert assert_true(true)
 
-    assert !vtest.assert_false(true)
-    assert vtest.assert_false(false)
+    assert !assert_false(true)
+    assert assert_false(false)
 }
 
 fn test_unescape_newline() {
     value := "1000\n2000"
 
-    assert vtest.assert_eq_str(vtest.unescape_newline(value), "1000\\n2000")
+    assert assert_eq_str(unescape_newline(value), "1000\\n2000")
 }
 
 fn test_build() {
@@ -26,8 +27,8 @@ fn test_build() {
     right := "1320\n1000"
 
     mut expected_msg := ""
-    expected_msg += "Left:  `1" + term.green("00") + "0\\n1000`" + vtest.newline_separator
-    expected_msg += "Right: `1" + term.red("32") + "0\\n1000`" + vtest.newline_separator
+    expected_msg += "Left:  `1" + term.green("00") + "0\\n1000`" + newline_separator
+    expected_msg += "Right: `1" + term.red("32") + "0\\n1000`" + newline_separator
 
-    assert vtest.assert_eq_str(expected_msg, vtest.build_diff(left, right))
+    assert assert_eq_str(expected_msg, build_diff(left, right))
 }
